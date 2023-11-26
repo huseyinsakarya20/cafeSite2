e129e1f35d1a200443a1c5a93877e6addf1c8513
Hüseyin Sakarya <sakaryahuseyin760@gmail.com>
2023-11-27 01:04:29 +0300

    style

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ const bar = document.querySelector(".bar");
 const dropdownMenu = document.querySelector(".dropdown_menu");
 let timeoutId;
 
-
 bar.onclick = function (){
     dropdownMenu.classList.toggle('open')
 
     timeoutId = setTimeout(function () {
         dropdownMenu.classList.remove('open');
-    }, 30000);
+    }, 10000);
 };
+dropdownMenu.addEventListener('click', function (event) {
+    if (event.target.tagName === 'A') {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(function () {
+            dropdownMenu.classList.remove('open');
+        }, 1);
+    }
+});
+
 
 
